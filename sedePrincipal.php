0fd74ac1c2e7a905526062db1ae2d364d8119b15
luis fernando cifuentes corba <luisferci12@gmail.com>
2021-01-30 12:46:11 -0500

    mas mejoras

--- sedePrincipal.php
+++ sedePrincipal.php
@@ -1,26 +1,20 @@
 <?php require_once 'views/header.php'; ?>
 <h1 class="titulo">Sede Principal</h1>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <img src="assets/img/img-principal.jpg" alt="" class="img-fluid sedep">
-        </div>
-    </div>
-</div>
 <h1 class="titulo">Encuentranos En<i class="fa fa-envira icon-location" aria-hidden="true"></i></h1>
-<div class="espacio2"></div>
 <div class="container">
     <div class="row">
         <div class="col-md-4">
             <div class="mapa">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3977.04118323057!2d-74.10053373573604!3d4.586631146667825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4c82826e5a99f2a4!2sCertificando!5e0!3m2!1ses!2sco!4v1611929153911!5m2!1ses!2sco" width="350" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <p>Transversal 18 BIS #13-36 sur  Frente del Caí RESTREPO
+                    Barrio restrepo
+                    Localidad Antonio Nariño
+                </p>
             </div>
         </div>
         <div class="col-md-4">
-        <p>Transversal 18 BIS #13-36 sur  Frente del Caí RESTREPO
-            Barrio restrepo
-            Localidad Antonio Nariño</p>
-        <p><span class="subtitulo">Web Site:</span> www.ferestsaludips.com</p>
+        <div class="espacio2"></div>
+            <img src="assets/img/img-principal.jpg" alt="" class="img-fluid">
         </div>
         <div class="col-md-4">
             <h2>Contacto Directo:</h2>
